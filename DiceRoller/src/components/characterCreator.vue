52f3eee0d4f7e52797d869b2846c332a6da50068
Sakvad <dxe8484@rit.edu>
2017-11-28 20:31:37 -0500

    Working on finding -infinity bug for 6 rolls

--- DiceRoller/src/components/characterCreator.vue
+++ DiceRoller/src/components/characterCreator.vue
@@ -3,7 +3,7 @@
     <p>Welcome to the character creator! Here you can choose the amount of D6 to roll (if your DM varies from the standard 4d6) and then you can put your rolls in the desired stat. You also have the option to use a point buy system to create your character.</p>
     <label>D6 Amount: <input type="number" min="3" max="15" v-model="times"/></label>
     <br/>
-    <button v-on:click="diceRoll()">Roll {{times}} D6</button>
+    <button v-on:click="diceRoll()">Six sums from {{times}} D6</button>
     <br/>
     <span id="statRolls"></span>
   </div>
@@ -44,27 +44,34 @@
           this.rollsArray.push(roll);
         }
         // this.displayRolls();
+        // this.diceSum();
+        for (let i = 0; i < 6; i++) {
+          this.diceSum();
+        }
         this.displaySums();
-        this.diceSum();
       },
       diceSum: function(){
         let sum = 0;
-        for (let i = 0; i < 6 ; i++) {
-          for (let j = 0; j < 3; j++) {
-            let highest = Math.max(...this.rollsArray);
-            let spot = this.rollsArray.indexOf(highest);
-            sum += highest;
-            this.rollsArray.splice(spot, 1);
-          }
-        this.sums.push(sum);
-        }
-        console.log(this.sums);
-        // for (let i = 0; i < 3; i++) {
-        //   let highest = Math.max(...this.rollsArray);
-        //   let spot = this.rollsArray.indexOf(highest);
-        //   sum += highest;
-        //   this.rollsArray.splice(spot, 1);
+        // for (let i = 0; i < 6 ; i++) {
+        //   for (let j = 0; j < 3; j++) {
+        //     let highest = Math.max(...this.rollsArray);
+        //     let spot = this.rollsArray.indexOf(highest);
+        //     sum += highest;
+        //     this.rollsArray.splice(spot, 1);
+        //     console.log(sum);
+        //   }
+        //   console.log(sum);
+        // this.sums.push(sum);
         // }
+        for (let i = 0; i < 3; i++) {
+          let highest = Math.max(...this.rollsArray);
+          let spot = this.rollsArray.indexOf(highest);
+          sum += highest;
+          this.rollsArray.splice(spot, 1);
+          // console.log(sum);
+        }
+        this.sums.push(sum);
+        // console.log(sum);
         // this.sums.push(sum);
       }
     },
