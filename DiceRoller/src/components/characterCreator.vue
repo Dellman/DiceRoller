ef44d8ed7e52bb3e31f0f34cf578a3ab4b50430a
Sakvad <dxe8484@rit.edu>
2017-12-07 16:21:31 -0500

    Dynamically add rows based on number of stats and properly reset the table.

--- DiceRoller/src/components/characterCreator.vue
+++ DiceRoller/src/components/characterCreator.vue
@@ -3,27 +3,27 @@
     <p>Welcome to the character creator! Here you can choose the amount of D6 to roll (if your DM varies from the standard 4d6) and then you can put your rolls in the desired stat. You also have the option to use a point buy system to create your character.</p>
     <button>Standard 4D6</button>
     <br />
-    <label>D6 Amount: <input type="number" min="1" max="8" v-model="numberOfD6"/></label>
+    <label>D6 Amount: <input type="number" min="1" max="8" v-model="amountOfD6"/></label>
     <br />
     <label>Sum Bonus: <input type="number" min="0" max="12" v-model="sumBonus"></label>
     <br />
-    <label >Amount of Stats: <input type="number" min="1" max="12" v-model="numberOfStats"></label>
+    <label >Amount of Stats: <input type="number" min="1" max="12" v-model="amountOfStats"></label>
     <br/>
-    <button v-on:click="diceRoll()">Sum of the three highest from {{numberOfD6}} D6</button>
+    <button v-on:click="diceRoll()">Sum of the three highest from {{amountOfD6}} D6</button>
     <br/>
     <span id="statRolls"></span>
     <table>
       <thead>
         Roll Results
       </thead>
-      <tr>
+      <tr id="tableNumbers">
         <th></th>
-        <th>Stat 1</th>
+        <!-- <th>Stat 1</th>
         <th>Stat 2</th>
         <th>Stat 3</th>
         <th>Stat 4</th>
         <th>Stat 5</th>
-        <th>Stat 6</th>
+        <th>Stat 6</th> -->
       </tr>
       <!-- <tr id="table-rolls">
         <th>Dice Results</th>
@@ -34,23 +34,23 @@
         <td></td>
         <td></td>
       </tr> -->
-      <tr id="table-sum">
+      <tr id="tableSum">
         <th>Stat number (sum)</th>
+        <!-- <td></td>
         <td></td>
         <td></td>
         <td></td>
         <td></td>
-        <td></td>
-        <td></td>
+        <td></td> -->
       </tr>
-      <tr id="table-mod">
+      <tr id="tableMod">
         <th>Modifier</th>
+        <!-- <td></td>
         <td></td>
         <td></td>
         <td></td>
         <td></td>
-        <td></td>
-        <td></td>
+        <td></td> -->
       </tr>
     </table>
   </div>
@@ -61,9 +61,9 @@
     name: 'Creator',
     data(){
       return{
-        numberOfD6: 4,
+        amountOfD6: 4,
         sumBonus: 0,
-        numberOfStats: 6,
+        amountOfStats: 6,
         rolls: [],
         sums: [],
         modifiers: [],
@@ -72,51 +72,73 @@
     },
     methods: {
       emptyTable: function(){
-        let tableSum = document.getElementById('table-sum');
-        for (let i = 1; i < tableSum.children.length; i++) {
-          tableSum.children[i].textContent = "";
+        const emptyRows = (id) =>{
+          let parent = document.getElementById(id);
+          while(parent.childNodes.length > 1){
+            parent.removeChild(parent.lastChild);
+          }
         }
-        let tableMod = document.getElementById('table-mod');
-        for (let i = 1; i < tableMod.children.length; i++) {
-          tableMod.children[i].textContent = "";
+        emptyRows('tableNumbers');
+        emptyRows('tableSum');
+        emptyRows('tableMod');
+      },
+      createTableRows: function(){
+        let th = document.createElement('th');
+        let td = document.createElement('td');
+
+        const createElements = (element, id) => {
+          for (let i = 1; i < parseInt(this.amountOfStats) + 1; i++) {
+            let createdEle = document.createElement(element);
+            let parent = document.getElementById(id);
+            if (element == 'th') {
+              createdEle.textContent = i;
+            }
+            parent.append(createdEle);
+          }
         }
+        createElements('th', 'tableNumbers');
+        createElements('td', 'tableSum');
+        createElements('td', 'tableMod');
       },
       displayResults: function(){
         // let tableRolls = document.getElementById('table-rolls');
         // for (let i = 1; i < tableRolls.children.length; i++) {
         //   tableRolls.children[i].append(this.rollObjs[i-1].rolls);
         // }
-        let tableSum = document.getElementById('table-sum');
-        for (let i = 1; i < tableSum.children.length; i++) {
-          tableSum.children[i].append(this.rollObjs[i - 1].sum);
-        }
-        let tableMod = document.getElementById('table-mod');
-        for (let i = 1; i < tableMod.children.length; i++) {
-          tableMod.children[i].append(this.rollObjs[i - 1].modifier);
-        }
+        // let tableSum = document.getElementById('table-sum');
+        // for (let i = 1; i < tableSum.children.length; i++) {
+        //   tableSum.children[i].append(this.rollObjs[i - 1].sum);
+        //   console.log("Sum" + i, this.rollObjs[i - 1]);
+        // }
+        // for (var i = 1; i < this.rollObjs.length; i++) {
+        // }
+        // let tableMod = document.getElementById('table-mod');
+        // for (let i = 1; i < tableMod.children.length; i++) {
+        //    tableMod.children[i].append(this.rollObjs[i - 1].modifier);
+        //   console.log("Modifier" + i, this.rollObjs[i - 1]);
+        // }
       },
-      displayRolls: function(rolls){
-        let tableRolls = document.getElementById('table-rolls');
-        for (var i = 1; i < tableRolls.children.length; i++) {
-        }
-      },
+      // displayRolls: function(rolls){
+      //   let tableRolls = document.getElementById('table-rolls');
+      //   for (var i = 1; i < tableRolls.children.length; i++) {
+      //   }
+      // },
       diceRoll: function(){
         this.emptyTable();
         this.rollObjs.length = 0;
-        for (let i = 0; i < this.numberOfStats; i++) {
+        for (let i = 0; i < this.amountOfStats; i++) {
           let rollObj = {
             rolls: [],
             sum: 0,
             modifier: 0
           }
-          for (let j = 0; j < this.numberOfD6; j++) {
+          for (let j = 0; j < this.amountOfD6; j++) {
             let roll = Math.floor(Math.random() * 6 + 1);
             rollObj.rolls.push(roll);
           }
           this.rollObjs.push(rollObj);
         }
         this.diceSum();
-        this.displayResults();
       },
       diceSum: function(){
         for (let i = 0; i < this.rollObjs.length; i++) {
@@ -124,11 +146,10 @@
           let tempRolls = [];
           tempRolls = [...this.rollObjs[i].rolls];
           let maxRolls = 3;
-          if (this.numberOfD6 < 3) {
-            maxRolls = this.numberOfD6;
+          if (this.amountOfD6 < 3) {
+            maxRolls = this.amountOfD6;
           }
           for (let j = 0; j < maxRolls; j++) {
-            console.log(maxRolls);
             let highest = Math.max(...tempRolls);
             let spot = tempRolls.indexOf(highest);
             sum += highest;
@@ -144,6 +165,7 @@
           mod = Math.floor((this.rollObjs[i].sum - 10)/2);
           this.rollObjs[i].modifier = mod;
         }
+        this.createTableRows();
       }
     },
   }
