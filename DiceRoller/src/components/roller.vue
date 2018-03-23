ed7ebea988e7b886b47593166d7df2f00feea2b5
Dellman <dellman307307@gmail.com>
2018-03-23 13:31:54 -0400

    Added styling for allRolls using CSS grid.

--- DiceRoller/src/components/roller.vue
+++ DiceRoller/src/components/roller.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="section">
+  <div class="main">
+    <div class="interaction">
       <h1>Dice Roller</h1>
       <p>Welcome to the dice roller! Here you can roll dice for whatever reason! Rolling on advantage means rolling twice and taking the higher value, while disadvantage means taking the lower value (the ignored value will have a strike-through). You can also roll multiple times by changing the roll number, and the modifier will be added to the original roll. A green number means a critical success when using the D20, while a red means a ciritcal failure. If you are looking to use this to simply create a character, check the <router-link to="/Creator">character creator page!</router-link></p>
       <span>
@@ -13,7 +13,7 @@
         <button v-on:click="roll(100)">D100</button>
         <br />
         <label>Custom Number: <input type="number" placeholder="Custom Number" min="2" v-model="customNumber"></label>
-        <button v-on:click="roll(customNumber)">{{ customNumber }}</button>
+        <button v-on:click="roll(customNumber)">D{{ customNumber }}</button>
       </span>
 
       <br />
@@ -28,22 +28,26 @@
       <br />
       <label>Modifier: <input type="number" v-model="modifier" min="-20" max="20"></label>
       <br />
-      <span id="rolls"></span>
 
       <!-- Most recent rolls -->
       <ul class="rolls">
         <li v-for="roll in rolls" :key="roll.id" v-bind:class="{'nat1': roll.isNat1, 'nat20': roll.isNat20, 'strike': roll.strike}">
           {{ roll.modRoll }} <!-- Final value -->
-        </li>
+        </li>        
       </ul>
 
       <span>Total: {{ total }}</span>
     </div>
+
     <!-- All rolls -->
-    <div class="all-rolls section">
+    <div class="all-rolls">
       <button @click="clearAllRolls()">Clear Rolls</button>
-      <li v-for="roll in allRolls" :key="roll.id" v-bind:class="{'nat1': roll.isNat1, 'nat20': roll.isNat20, 'strike': roll.strike}">
-        {{ roll.modRoll }} <!-- Final value -->
+      <li class="all-rolls-container" v-for="roll in allRolls" :key="roll.id">
+        <span class="all-rolls-desc" v-if="roll.id === 0">{{ roll.times }}D{{ roll.dice }} + {{ roll.modRoll - roll.orgRoll }}<span v-if="roll.times > 1">, Total: {{ roll.total }} </span><br />
+          <!-- <span v-if="roll.times === 1 && roll.rollType === 'normal'">Roll:</span> -->
+          <!-- <span v-else>Rolls <span v-if="roll.rollType !== 'normal'">on {{ roll.rollType }}:</span> </span>           -->
+        </span> <!-- Only show the roll descriptor once -->
+        <span class="all-rolls-roll" v-bind:class="{'nat1': roll.isNat1, 'nat20': roll.isNat20, 'strike': roll.strike}">{{ roll.modRoll }} </span><!-- Final value -->
       </li>
     </div>
     
@@ -80,13 +84,13 @@ export default {
             modRoll: parseInt(roll + parseInt(this.modifier)),
             isNat1: false,
             isNat20: false,
-            strike: false
+            rollType: this.rollType,
+            strike: false,
+            times: this.times
           };
           this.rolls.push(rollO);
         }
         this.allRolls = this.allRolls.concat(this.rolls);
-        // this.allRolls.push(this.rolls);
-        console.log(this.allRolls);        
       }
       //roll twice as many times if rolling on advantage or disadvantage
       this.rollType === "advantage" || this.rollType === "disadvantage" ? rollDice(2) : rollDice(1);
@@ -121,7 +125,7 @@ export default {
         const roll = this.rolls[i];
         const nextRoll = this.rolls[i+1];
         if(roll.modRoll < nextRoll.modRoll || roll.modRoll === nextRoll.modRoll){
-          if(this.rollType === "advantage") {            
+          if(this.rollType === "advantage") {          
             roll.strike = true;   
           }
           else if(this.rollType === "disadvantage"){
@@ -138,7 +142,7 @@ export default {
         }
       }
       this.addRolls();
-    }, 
+    },
     addRolls: function(){ // add to computed when done?
       this.total = 0;
       if(this.times === 1){
@@ -152,6 +156,8 @@ export default {
           }
         }
       }
+      this.rolls[0].total = this.total;
+      console.log(this.rolls[0].total);
     }
   },
   computed: {
@@ -178,6 +184,11 @@ body{
   background-image: url("../assets/parchment.jpg");
   font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
+.main{
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: minmax(100px, auto);
+}
 h1, h2 {
   font-weight: normal;
 }
@@ -192,9 +203,6 @@ li {
 .rolls li{
   margin: 0 3px;
 }
-#rolls, #rolls ul li{
-  display: inline;
-}
 .nat1{
   color: red;
 }
@@ -204,16 +212,26 @@ li {
 .strike{
   text-decoration: line-through;
 }
-.section{
-  width: 50%;
-}
 .all-rolls{
-  background-color: gray;
-  width: 20%;
+  background-color: lightgray;
+  height: 400px;
+  overflow-y: auto;
 }
 .all-rolls button{
-  display: block;
-  margin: 0 auto;
+  position: fixed;
+  left: 75%;
 }
-
+.all-rolls-container{
+  display: inline;
+}
+.all-rolls-desc{
+  display: block;
+  margin: 5px 0;
+}
+.all-rolls-desc, .all-rolls-roll{
+  margin-left: 5px;
+}
+.all-rolls li{
+  margin: 0;
+}
 </style>
